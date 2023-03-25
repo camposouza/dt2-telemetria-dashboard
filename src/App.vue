a075cf3725786ec6206856d69f7de7a0af0c9add
camposouza <guilhermesouza0911@outlook.com>
2023-03-24 23:43:43 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+<body>
+<header>EQUIPE MILHAGEM</header>
+<div class="dashboard">
+    <div class="area-1">
+        <div class="velocimetro">
+            ENCODER
+        </div>
+        <div class="termometro">
+            LM35
+        </div>
+    </div>
+    <div class="area-tensao-corrente">
+        <div class="tensao-corrente">
+            9.05V / 4.41A
+        </div>
+        <div class="graficos">
+            GRAFICOS
+        </div>
+    </div>
+    <div class='area-wattmetro'>
+        <TheWattmeter />
+    </div>
+    <div class="area-mapa">MAPA</div>
+</div>
+</body>
+</template>
+
+<script>
+    import TheWattmeter from './components/TheWattmeter.vue';
+
+    export default {
+        name: 'App',
+        components: {
+            TheWattmeter
+        }
+    }
+</script>
+
+<style>
+@import './css/reset.css';
+header {
+    height: 50px;
+    background-color: aqua;
+}
+
+.dashboard {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 310px;
+}
+
+.area-1 {
+    background-color: blue;
+    display: grid;
+    grid-template-columns: 80% 20%;
+}
+
+.velocimetro {
+    background-color: rgb(57, 57, 167) ;
+}
+
+.termometro {
+    background-color: rgb(16, 16, 94);
+}
+
+.area-tensao-corrente {
+    background-color: red;
+    display: grid;
+    grid-template-rows: 20% 80%;
+}
+
+.tensao-corrente {
+    background-color: rgb(202, 47, 47);
+}
+
+
+.graficos {
+    background-color: rgb(170, 44, 44);
+}
+
+.area-mapa {
+    background-color: yellow;
+}
+</style>
---
+++ src/components/TheWattmeter.vue
@@ -0,0 +1,227 @@
+<template>
+<div class="speedometer-container">
+    <div class="speedometer-text">
+        <div class="static">Power</div>
+        <div class="dynamic">
+            <span class="watts">0</span>
+            <span class="unit">W</span>
+        </div>
+    </div>
+    <div class="center-point"></div>
+    <div class="speedometer-center-hide"></div>
+    <div class="speedometer-bottom-hide"></div>
+    <div class="arrow-container">
+        <div class="arrow-wrapper arrow-angle">
+            <div class="arrow"></div>
+        </div>
+    </div>
+    <div class="speedometer-scale speedometer-scale-1 "></div>
+    <div class="speedometer-scale speedometer-scale-2"></div>
+    <div class="speedometer-scale speedometer-scale-3"></div>
+    <div class="speedometer-scale speedometer-scale-4"></div>
+    <div class="speedometer-scale speedometer-scale-5"></div>
+    <div class="speedometer-scale speedometer-scale-6"></div>
+    <div class="speedometer-scale speedometer-scale-7"></div>
+    <div class="speedometer-scale speedometer-scale-8"></div>
+    <div class="speedometer-scale speedometer-scale-9"></div>
+    <div class="speedometer-scale speedometer-scale-10"></div>
+    <div class="speedometer-scale speedometer-scale-11"></div>
+    <div class="speedometer-scale speedometer-scale-12"></div>
+    <div class="speedometer-scale speedometer-scale-13"></div>
+    <div class="speedometer-scale speedometer-scale-14"></div>
+    <div class="speedometer-scale speedometer-scale-15"></div>
+    <div class="speedometer-scale speedometer-scale-16"></div>
+    <div class="speedometer-scale speedometer-scale-17"></div>
+    <div class="speedometer-scale speedometer-scale-18"></div>
+    <div class="speedometer-scale speedometer-scale-19"></div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'TheWattmeter '
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.speedometer-container {
+    width: 300px;
+    height: 300px;
+    border: 3px solid black;
+    border-radius: 50%;
+    margin: 50px auto;
+    position: relative;
+}
+
+.center-point {
+    width: 20px;
+    height: 20px;
+    background-color: black;
+    border-radius: 50%;
+    position: absolute;
+    top: 137px;
+    left: 137px;
+    z-index: 10;
+}
+
+.speedometer-scale {
+    width: 8px;
+    height: 280px;
+    background-color: black;
+    position: absolute;
+    left: 143px;
+    top: 7px;    
+}
+
+.speedometer-scale-1 {
+    transform: rotate(-90deg);
+}
+.speedometer-scale-2 {
+    transform: rotate(-80deg);
+}
+.speedometer-scale-3 {
+    transform: rotate(-70deg);
+}
+.speedometer-scale-4 {
+    transform: rotate(-60deg);
+}
+.speedometer-scale-5 {
+    transform: rotate(-50deg);
+}
+.speedometer-scale-6 {
+    transform: rotate(-40deg);
+}
+.speedometer-scale-7 {
+    transform: rotate(-30deg);
+}
+.speedometer-scale-8 {
+    transform: rotate(-20deg);
+}
+.speedometer-scale-9 {
+    transform: rotate(-10deg);
+}
+.speedometer-scale-10 {
+    transform: rotate(0deg);
+}
+.speedometer-scale-11 {
+    transform: rotate(10deg);
+}
+.speedometer-scale-12 {
+    transform: rotate(20deg);
+}
+.speedometer-scale-13 {
+    transform: rotate(30deg);
+}
+.speedometer-scale-14 {
+    transform: rotate(40deg);
+}
+.speedometer-scale-15 {
+    transform: rotate(50deg);
+}
+.speedometer-scale-16 {
+    transform: rotate(60deg);
+}
+.speedometer-scale-17 {
+    transform: rotate(70deg);
+}
+.speedometer-scale-18 {
+    transform: rotate(80deg);
+}
+.speedometer-scale-19 {
+    transform: rotate(90deg);
+    height: 244px;
+    top: 25px;
+    left: 161px;
+}
+
+.active-scale {
+    background-color: purple;
+}
+
+.speedometer-center-hide {
+    width: 250px;
+    height: 250px;
+    background-color: white;
+    border-radius: 50%;
+    position: absolute;
+    top: 22px;
+    left: 22px;
+    z-index: 9;
+}
+
+.speedometer-bottom-hide {
+    width: 320px;
+    height: 250px;
+    background-color: white;
+    position: absolute;
+    z-index: 11;
+    top: 160px;
+    left: -14px;
+    border-top: 1px solid;
+}
+
+.arrow-container {
+    width: 160px;
+    height: 160px;
+    background-color: transparent;
+    position: absolute;
+    z-index: 12;
+    top: 67px;
+    left: 67px;
+}
+
+.arrow-wrapper {
+    width: 160px;
+    height: 160px;
+    background-color: transparent;
+    position: relative;
+    transition: all 0.4s;
+}
+
+.arrow {
+    width: 110px;
+    height: 4px;
+    background-color: purple;
+    position: absolute;
+    top: 78px;
+    left: -30px;
+}
+
+.arrow-angle {
+    transform: rotate(0deg);
+}
+
+.speedometer-text {
+    width: 180px;
+    position: absolute;
+    z-index: 20;
+    left: 58px;
+    top: 60px;
+    text-align: center;
+    font-weight: bold;
+}
+
+.static {
+    font-size: 18px;
+}
+
+.dynamic {
+    margin-top: 10px;
+}
+
+.watts {
+    font-size: 32px;
+}
+
+.unit {
+    font-size: 14px;
+    margin-left: 5px;
+}
+</style>
